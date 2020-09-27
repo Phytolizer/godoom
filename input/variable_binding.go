bdb1c9e2308a1b4a3fc43ea03970ee850a4012d6
onContentStop <mariorules24@gmail.com>
2020-09-27 15:46:51 -0500

    Implement input.BindInputVariables

--- input/variable_binding.go
+++ input/variable_binding.go
@@ -1,5 +1,10 @@
 package input
 
+import "godoom/configuration"
+
 func BindInputVariables() {
-	// TODO
+	configuration.BindFloatVariable("mouse_acceleration", &MouseAcceleration)
+	configuration.BindIntVariable("mouse_threshold", &MouseThreshold)
+	configuration.BindBoolVariable("vanilla_keyboard_mapping", &VanillaKeyboardMapping)
+	configuration.BindBoolVariable("novert", &NoVert)
 }
---
+++ input/vars.go
@@ -0,0 +1,8 @@
+package input
+
+var (
+	MouseAcceleration      = 2.0
+	MouseThreshold         = 10
+	VanillaKeyboardMapping = true
+	NoVert                 = false
+)
