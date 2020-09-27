3573aec4e483ae03ce4e9f8878931b7aa18147b7
onContentStop <mariorules24@gmail.com>
2020-09-27 13:01:57 -0500

    Add joystick/sound var binding to main fn

--- doom/variable_binding.go
+++ doom/variable_binding.go
@@ -2,11 +2,14 @@ package doom
 
 import (
 	"godoom/input"
+	"godoom/joystick"
+	"godoom/sound"
 	"godoom/video"
 )
 
 func bindVariables() {
 	input.BindInputVariables()
 	video.BindVideoVariables()
-
+	joystick.BindJoystickVariables()
+	sound.BindSoundVariables()
 }
---
+++ sound/variable_binding.go
@@ -0,0 +1,5 @@
+package sound
+
+func BindSoundVariables() {
+	// TODO
+}
