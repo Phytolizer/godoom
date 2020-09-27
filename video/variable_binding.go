f3c796eea46ee78e3d977c3d4e343b1d22afca2a
onContentStop <mariorules24@gmail.com>
2020-09-27 16:07:18 -0500

    Implement video.BindVideoVariables

--- video/variable_binding.go
+++ video/variable_binding.go
@@ -1,5 +1,24 @@
 package video
 
+import "godoom/configuration"
+
 func BindVideoVariables() {
-	// TODO
+	configuration.BindBoolVariable("use_mouse", &UseMouse)
+	configuration.BindBoolVariable("fullscreen", &Fullscreen)
+	configuration.BindIntVariable("video_display", &VideoDisplay)
+	configuration.BindBoolVariable("aspect_ratio_correct", &AspectRatioCorrect)
+	configuration.BindBoolVariable("integer_scaling", &IntegerScaling)
+	configuration.BindBoolVariable("vga_porch_flash", &VgaPorchFlash)
+	configuration.BindIntVariable("startup_delay", &StartupDelay)
+	configuration.BindIntVariable("fullscreen_width", &FullscreenWidth)
+	configuration.BindIntVariable("fullscreen_height", &FullscreenHeight)
+	configuration.BindBoolVariable("force_software_renderer", &ForceSoftwareRenderer)
+	configuration.BindIntVariable("max_scaling_buffer_pixels", &MaxScalingBufferPixels)
+	configuration.BindIntVariable("window_width", &WindowWidth)
+	configuration.BindIntVariable("window_height", &WindowHeight)
+	configuration.BindBoolVariable("grabmouse", &GrabMouse)
+	configuration.BindStringVariable("video_driver", &VideoDriver)
+	configuration.BindStringVariable("window_position", &WindowPosition)
+	configuration.BindIntVariable("usegamma", &UseGamma)
+	configuration.BindBoolVariable("png_screenshots", &PngScreenshots)
 }
---
+++ video/vars.go
@@ -0,0 +1,22 @@
+package video
+
+var (
+	UseMouse               = true
+	Fullscreen             = true
+	VideoDisplay           = 0
+	AspectRatioCorrect     = true
+	IntegerScaling         = false
+	VgaPorchFlash          = false
+	StartupDelay           = 1000
+	FullscreenWidth        = 0
+	FullscreenHeight       = 0
+	ForceSoftwareRenderer  = false
+	MaxScalingBufferPixels = 16000000
+	WindowWidth            = 800
+	WindowHeight           = 600
+	GrabMouse              = true
+	VideoDriver            = ""
+	WindowPosition         = "center"
+	UseGamma               = 0
+	PngScreenshots         = false
+)
